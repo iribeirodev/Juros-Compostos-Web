6e5094c5f0a64efbd55d3bf36f0c391652bf8103
Itamar.Ribeiro <ribeirojr.itamar@gmail.com>
2025-03-13 10:28:31 -0300

    Criando documentação swagger

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	_ "iribeirodev/juros-compostos-web/docs"
 	"log"
 	"math"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 type Entrada struct {
@@ -20,19 +23,52 @@ type Montante struct {
 	MA  string `json:"montante_acumulado"`
 }
 
+// @title Juros Compostos API
+// @version 1.0
+// @description Exemplo de API com Swagger no Gin
 func main() {
 	router := gin.Default()
-	router.GET("/", ola)
-	router.POST("/calculo", getCalculo)
 
-	router.Run("localhost:8080")
+	// Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// Ping
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/", ping)
+	router.POST("/compostos", getCalculoCompostos)
+
+	router.Run(":8080")
 }
 
-func ola(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "ola")
+// Retorna uma resposta "pong" para verificar se a API está funcionando corretamente
+// @Summary Verifica o status da API
+// @Description Retorna uma resposta "pong" indicando que a API está ativa
+// @Tags HealthCheck
+// @Produce json
+// @Success 200 {object} map[string]string "Resposta com mensagem 'pong'"
+// @Router /ping [get]
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
 
-func getCalculo(c *gin.Context) {
+// Realiza o cálculo de juros compostos com base nos valores de entrada
+// @Summary Calcula juros compostos
+// @Description Recebe valores de entrada e retorna o montante acumulado ao longo do tempo
+// @Tags Calculo
+// @Accept json
+// @Produce json
+// @Param input body Entrada true "Valores de entrada para o cálculo"
+// @Success 200 {array} Montante "Lista de montantes calculados por mês"
+// @Failure 400 {object} map[string]string "Mensagem de erro para entrada inválida"
+// @Router /compostos [post]
+func getCalculoCompostos(c *gin.Context) {
 	var input Entrada
 	if err := c.BindJSON(&input); err != nil {
 		log.Printf("Erro ao vincular JSON -> %v", err.Error())
